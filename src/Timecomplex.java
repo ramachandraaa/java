ec38b87dcdf480fb7fb0aa2ee7a16e78e4f33cab
nitin_shatkar <nitinshetkar53@gmail.com>
2024-10-11 10:50:08 +0530

    updated

--- src/Timecomplex.java
+++ src/Timecomplex.java
@@ -1,2 +1,19 @@
 public class Timecomplex {
+    public static void main(String[] args)
+    {
+      Timecomplex n=new Timecomplex();
+      System.out.println(n.sum(8));
+    }
+    public int sum(int n)
+    {
+        int sum=0;
+     //   return n*(n+1)/2;//less time of compilation
+        for(int i=0;i<n;i++)
+        {
+            sum+=i;
+
+       }
+return sum;
+    }
+
 }
