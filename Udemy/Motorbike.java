c83d8a4fcff6dda3c810cf172a1e759221b0d0e8
ramachandraaa <159449668+ramachandraaa@users.noreply.github.com>
2025-02-03 10:14:27 +0530

    Hey Great going!!!

---
+++ Udemy/Motorbike.java
@@ -0,0 +1,10 @@
+package Udemy;
+
+public class Motorbike {
+    public void Start()
+
+{
+    System.out.println("Hey the Bike Has Started!!");
+}
+    
+}
---
+++ Udemy/MotorbikeRunner.java
@@ -0,0 +1,13 @@
+package Udemy;
+
+public class MotorbikeRunner {
+    public static void main(String[] args) {
+        Motorbike hero=new Motorbike();
+        Motorbike honda=new Motorbike(); 
+        hero.Start();
+        honda.Start();
+
+    }
+
+    
+}
