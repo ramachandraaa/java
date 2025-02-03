7cd2e888e672b1b69a26b518be9cf88b72cd3e00
ramachandraaa <159449668+ramachandraaa@users.noreply.github.com>
2025-02-03 10:35:40 +0530

    Encaptulation!!

--- Udemy/Motorbike.java
+++ Udemy/Motorbike.java
@@ -1,10 +1,12 @@
 package Udemy;
 
 public class Motorbike {
+   private int Speed;
     public void Start()
 
 {
     System.out.println("Hey the Bike Has Started!!");
+
 }
     
 }
--- Udemy/MotorbikeRunner.java
+++ Udemy/MotorbikeRunner.java
@@ -5,7 +5,10 @@ public class MotorbikeRunner {
         Motorbike hero=new Motorbike();
         Motorbike honda=new Motorbike(); 
         hero.Start();
-        honda.Start();
+        honda.Start(); 
+     hero.Speed=100;
+     honda.Speed=200;
+     System.out.println(hero.Speed);
 
     }
 
