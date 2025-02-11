8a025a70d85768c98217a5cf3e855a75f32615a0
ramachandraaa <159449668+ramachandraaa@users.noreply.github.com>
2025-02-11 13:27:17 +0530

    Some work must be done!!

---
+++ DSA/arrays/leaderarray.java
@@ -0,0 +1,33 @@
+package DSA.arrays;
+import java.util.Scanner;
+public class leaderarray {
+    public static void main(String[] args) {
+        int [] a=new int[100];
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter N");
+int n=sc.nextInt();
+System.out.println("Enter the Numbers:");
+for(int i=0;i<n;i++)
+{
+    a[i]=sc.nextInt();    
+  
+}   
+int i=0;
+int j=n-1;
+while(j>i)
+{
+    if(a[i]>a[j])
+    {
+        j--;
+        continue;
+    }
+
+}
+if(j==i)
+{
+    System.out.println(a[i]);
+}
+
+    }
+
+}
