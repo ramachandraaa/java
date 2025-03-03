5e51d85cb6aba8488ae4ca5a1806b12344abe59e
ramachandraaa <159449668+ramachandraaa@users.noreply.github.com>
2025-03-03 21:47:10 +0530

    hi

---
+++ DSA/arrays/longest.java
@@ -0,0 +1,22 @@
+package DSA.arrays;
+//this program prints he longst subaarray
+
+public class longest {
+    public static void main(String[] args) {
+        int i;
+     int arr[]=new int[]{2,3,5,1,9};
+       for( i=0;i<arr.length;i++)
+       {
+        for(int j=0;j<arr.length;j++)
+        {
+          
+        }
+        //Ram
+
+
+
+       }
+    }
+
+    
+}
