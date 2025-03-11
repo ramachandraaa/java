c21014424cf1a47ee4ceb8cd96f21f672b7cc7b9
ramachandraaa <159449668+ramachandraaa@users.noreply.github.com>
2025-03-11 10:27:19 +0530

    this is the basic stack operation

---
+++ DSA/Stacks_AND_Ques/stackbasic.java
@@ -0,0 +1,18 @@
+package Stacks_AND_Ques;
+
+import java.util.Stack;
+
+public class stackbasic {
+    public static void main(String[] args) {
+        Stack<Integer> stack=new Stack<>();
+        System.out.println(stack.empty());
+        stack.push(10);
+        System.out.println(stack.empty());
+        System.out.println(stack.pop());
+        System.out.println(stack.empty());
+        //System.out.println(stack.pop());
+
+
+
+    }
+}
